cdd10b3f4bc660e78399d7e7d91891dfab964645
Dare Ryan <dareryan@me.com>
2026-02-18 20:05:47 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Dare on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
